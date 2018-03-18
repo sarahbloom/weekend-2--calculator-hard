180843bc78f8598bbb03990f41e16c8cdf65e891
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2018-03-18 13:57:29 -0500

    stored first digit to object

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,14 +1,32 @@
 let storedNumber;
+let firstNumber = {};
 
 $(document).ready(readyNow);
 
 function readyNow(){
     console.log('jQuery loaded');
-    $('.numberButtons').on('click', firstNumber)
+    clickListeners();
 }
 
-function firstNumber(){
-    console.log("success!");
-    let storedNumber = $(this).data("number")
-    console.log(storedNumber);
+function clickListeners(){
+    $('.numberButtons').on('click', submitNumber);
+    $('.calculate').on('click', submitOperator);
 }
+
+function submitNumber(){
+    let storedNumber = $(this).data("number");
+    console.log(storedNumber);
+    firstNumber = { firstNumber: storedNumber };
+    console.log(firstNumber);
+}
+
+function submitOperator() {
+    storedNumber = "";
+    operator = $(this).data("operator");
+    console.log(operator);   
+    return operator;
+}
+
+
+
+    
